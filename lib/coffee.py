a412f48d05fce772c12ea33a5ff598941caf1308
amandalocher <akent22000@gmail.com>
2025-05-09 11:04:12 -0400

    completed lab

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    ##get
+    @property
+    def size(self):
+        return self._size
+
+    ##get
+    @size.setter
+    def size(self, size):
+        if size == "Small" or size == "Medium" or size == "Large":
+            # breakpoint()
+            self._size = size
+            print(size)
+        else:
+            # breakpoint()
+            print(
+                "size must be Small, Medium, or Large"
+            )
+
+    # @classmethod - ob oriented go back to lessons
+    # revist instance lesson
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here’s a tip!")
